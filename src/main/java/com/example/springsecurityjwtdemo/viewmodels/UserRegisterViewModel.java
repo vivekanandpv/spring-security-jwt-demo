c174226a3ae9199fbe43dcc1b1202bd135a1a7f9
vivek <vivek@athenaeum.in>
2021-09-28 08:37:03 +0530

    done

---
+++ src/main/java/com/example/springsecurityjwtdemo/viewmodels/UserRegisterViewModel.java
@@ -0,0 +1,61 @@
+package com.example.springsecurityjwtdemo.viewmodels;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class UserRegisterViewModel {
+    private int id;
+    private String email;
+    private String password;
+    private String secretQuestion;
+    private String secretAnswer;
+    private List<String> roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecretQuestion() {
+        return secretQuestion;
+    }
+
+    public void setSecretQuestion(String secretQuestion) {
+        this.secretQuestion = secretQuestion;
+    }
+
+    public String getSecretAnswer() {
+        return secretAnswer;
+    }
+
+    public void setSecretAnswer(String secretAnswer) {
+        this.secretAnswer = secretAnswer;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
