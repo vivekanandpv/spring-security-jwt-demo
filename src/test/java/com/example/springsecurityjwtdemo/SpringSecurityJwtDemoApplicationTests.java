8b6146c2ac3f8d37c448928edaf596762161a951
vivek <vivek@athenaeum.in>
2021-09-28 06:35:50 +0530

    initial commit

---
+++ src/test/java/com/example/springsecurityjwtdemo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
