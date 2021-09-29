bb17ef75f9b77d68367d234d29d7884252961432
vivek <vivek@athenaeum.in>
2021-09-29 06:21:36 +0530

    done

--- src/main/java/com/example/springsecurityjwtdemo/controllers/SampleController.java
+++ src/main/java/com/example/springsecurityjwtdemo/controllers/SampleController.java
@@ -1,6 +1,8 @@
 package com.example.springsecurityjwtdemo.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
     @GetMapping("authenticated-route")
     public String getAuthenticatedRoute() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return "authenticated route ok";
     }
 
